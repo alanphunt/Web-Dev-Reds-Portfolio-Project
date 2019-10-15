fdbb29765f8fee0250aad0c3b7722af7d93c1944
Alan Hunt <Alan Hunt@10.135.122.36>
2019-10-14 22:06:50 -0500

    Ready for mobile nav!

---
+++ humpf/js/scripts.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+	document.getElementsByClassName("mobilemenutrigger")[0].addEventListener("click", mobileMenuFunc);
+	document.getElementsByClassName("exit")[0].addEventListener("click", closeFunc);
+}
+
+function mobileMenuFunc() {
+	var mobile = document.getElementById("mobilemenucontainer");
+	mobile.style.display = "flex";
+	mobile.className = "fadeinClass";
+	document.body.style.overflow = "hidden";
+}
+
+function closeFunc() {
+	var mobile = document.getElementById("mobilemenucontainer");
+	mobile.style.display = "none";
+	document.body.style.overflow = "visible";
+}
